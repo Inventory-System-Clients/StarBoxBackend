ad6d4ba788cb190825231b36181d900a3f804158
Tuco2006 <mateussmcruz@icloud.com>
2026-02-18 14:39:28 -0300

    ajustando routes

--- src/routes/maquina.routes.js
+++ src/routes/maquina.routes.js
@@ -17,29 +17,33 @@ import { problemaMaquina } from "../controllers/movimentacaoController.js";
 
 const router = express.Router();
 
+
 router.get("/", autenticar, listarMaquinas);
 router.get("/:id", autenticar, obterMaquina);
 router.get("/:id/estoque", autenticar, obterEstoqueAtual);
 router.get("/:id/problema", autenticar, problemaMaquina);
 // Endpoint para cálculo automático
 router.get("/:id/calcular-quantidade", autenticar, calcularQuantidadeAtual);
+router.post(
   "/",
   autenticar,
   autorizar(["ADMIN"]),
   registrarLog("CRIAR_MAQUINA", "Maquina"),
-  criarMaquina,
+  criarMaquina
 );
+router.put(
   "/:id",
   autenticar,
   autorizar(["ADMIN"]),
   registrarLog("EDITAR_MAQUINA", "Maquina"),
-  atualizarMaquina,
+  atualizarMaquina
 );
+router.delete(
   "/:id",
   autenticar,
   autorizar(["ADMIN"]),
   registrarLog("DELETAR_MAQUINA", "Maquina"),
-  deletarMaquina,
+  deletarMaquina
 );
 
 export default router;
