5d908981866a6d0d2be19eb3f46ed72dbc86332b
Gabriel <gakirakiyohara@gmail.com>
2026-04-13 21:31:22 -0300

    edição aparecer somente o nescessário

--- src/routes/produto.routes.js
+++ src/routes/produto.routes.js
@@ -1,22 +1,19 @@
 import express from "express";
 import {
   listarProdutos,
+  listarProdutosComEstoque,
   obterProduto,
   criarProduto,
   atualizarProduto,
   deletarProduto,
   listarCategorias,
 } from "../controllers/produtoController.js";
-import {
-  autenticar,
-  autorizar,
-  registrarLog,
-} from "../middlewares/auth.js";
+import { autenticar, autorizar, registrarLog } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-
 router.get("/", autenticar, listarProdutos);
+router.get("/com-estoque", autenticar, listarProdutosComEstoque);
 router.get("/categorias", autenticar, listarCategorias);
 router.get("/:id", autenticar, obterProduto);
 router.post(
@@ -24,21 +21,21 @@ router.post(
   autenticar,
   autorizar(["ADMIN"]),
   registrarLog("CRIAR_PRODUTO", "Produto"),
-  criarProduto
+  criarProduto,
 );
 router.put(
   "/:id",
   autenticar,
   autorizar(["ADMIN"]),
   registrarLog("EDITAR_PRODUTO", "Produto"),
-  atualizarProduto
+  atualizarProduto,
 );
 router.delete(
   "/:id",
   autenticar,
   autorizar(["ADMIN"]),
   registrarLog("DELETAR_PRODUTO", "Produto"),
-  deletarProduto
+  deletarProduto,
 );
 
 export default router;
